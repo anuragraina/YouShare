14dc7e65f4f749153cccb108088b97cb2be246bf
Anurag Raina <rainakalhan90@gmail.com>
2020-04-29 03:06:14 +0530

    Add authentication.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express'),
       port = 3000,
       bodyParser = require("body-parser"),
       mongoose = require("mongoose"),
+      passport = require("passport"),
+      LocalStrategy = require("passport-local"),
       Food = require("./models/foods"),
       Comment = require("./models/comments"),
+      User = require("./models/users"),
       seedDb = require("./seeds");
 
 
@@ -16,17 +19,38 @@ app.use(express.static(__dirname+'/public'));
 seedDb();
 
 //-------------------------------------
-//        INITIAL ROUTES
+//        PASSPORT SETUP
+//-------------------------------------
+app.use(require("express-session")({
+    secret: "Crack the password please...",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    next();
+})
+
+//-------------------------------------
+//              ROUTES
 //-------------------------------------
 app.get('/',(req,res)=>res.render('landing'));
 
 app.get('/foods',(req,res)=>{
+
+    console.log(req.user);
     Food.find({},(err,allFoods)=>{
         if(err){
             console.log("Error finding food!!!");
         }
         else{
-            res.render('foods/foods',{foods:allFoods})
+            res.render('foods/foods',{foods:allFoods});
         }
     })
 });
@@ -38,10 +62,10 @@ app.post('/foods',(req,res)=>{
             console.log("Error adding food!!!");
         }
         else{
-            console.log("Added food successfully...")
+            console.log("Added food successfully...");
         }
     })
-    res.redirect('foods/foods');
+    res.redirect('/foods');
 });
 
 app.get('/foods/new',(req,res)=>res.render("foods/new"));
@@ -51,7 +75,7 @@ app.get("/foods/:id",(req,res)=>{
     Food.findById(req.params.id).populate("comments").exec((err,foundFood)=>{
         if(err){
             console.log("Food item not found!!!");
-            res.redirect("foods/foods");
+            res.redirect("/foods");
         }
         else{
             res.render('foods/show',{food:foundFood});
@@ -59,7 +83,7 @@ app.get("/foods/:id",(req,res)=>{
     })
 });
 
-app.get("/foods/:id/comments/new",(req,res)=>{
+app.get("/foods/:id/comments/new",isLoggedIn,(req,res)=>{
 
     Food.findById(req.params.id,(err,foundFood)=>{
         if(err)
@@ -69,9 +93,9 @@ app.get("/foods/:id/comments/new",(req,res)=>{
             res.render("comments/new",{food:foundFood});
         }
     })
-})
+});
 
-app.post("/foods/:id/comments",(req,res)=>{
+app.post("/foods/:id/comments",isLoggedIn,(req,res)=>{
     
     Food.findById(req.params.id,(err,foundFood)=>{
         if(err)
@@ -86,7 +110,46 @@ app.post("/foods/:id/comments",(req,res)=>{
             })
         }
     })
-})
+});
 
+//-------------------------------------
+//           AUTH ROUTES
+//-------------------------------------
+app.get("/register",(req,res)=>res.render("register"));
+
+app.post("/register",(req,res)=>{
+    const newUser= new User({username:req.body.username});
+    
+    User.register(newUser,req.body.password,(err,addedUser)=>{
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req,res,()=>res.redirect("/foods"));
+        }
+    })
+});
+
+app.get("/login",(req,res)=>res.render("login"));
+
+app.post("/login",passport.authenticate("local",
+    {
+        successRedirect:"/foods",
+        failureRedirect:"/login"
+    }),(req,res)=>{});
+
+app.get("/logout",(req,res)=>{
+    req.logOut();
+    res.redirect("/foods");
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated())
+    return next();
+
+    else
+    res.redirect("/login");
+}
 
 app.listen(port,()=>console.log('YouShare server running on port '+port+'...'));
